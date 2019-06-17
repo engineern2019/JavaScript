d26999eb91c789929eeb69c5724017592a5dcb0f
engineern2019 <alidawood15@gmail.com>
2019-06-17 12:57:08 +0100

    addition

--- calculator.js
+++ calculator.js
@@ -1,12 +1,19 @@
-function add(a,b) {
-    console.log (a + b)
+function calc(operator) {
+    const a = parseInt (document.getElementById('inputA').value);
+    const b = parseInt (document.getElementById('inputB').value);
+    const c = a + b;
+
+    if (operator === '+') {
+        add(a + b);
+    } else if (operator === '-') {
+        subtract(a - b);
+    } else if (operator === '*') {
+        multiply(a * b );
+    } else if (operator === '/') {
+        divide(a / b);
+    }
 }
-function subtract(a,b) {
-    console.log (a - b)
-}
-function multiply(a,b) {
-    console.log (a * b)
-}
-function divide(a,b) {
-    console.log (a / b)
+
+function add(a, b) {
+    console.log(a + b);
 }
